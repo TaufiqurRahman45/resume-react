53004069150a142d5367a405a18e382c7f560d8c
LAPTOP-6OLBG1GR\User <toufiqurrahman45@gmail.com>
2020-10-18 21:50:07 +0800

    Updates

---
+++ precache-manifest.4b2d4e53b31a2a90eefd2819f1e00d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae18f8127eefc5462b5deb319811c3f",
+    "url": "/resume-react/index.html"
+  },
+  {
+    "revision": "4f9f89def61d4102e443",
+    "url": "/resume-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1699fa43513f73585abc",
+    "url": "/resume-react/static/js/2.1a1d116f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resume-react/static/js/2.1a1d116f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9f89def61d4102e443",
+    "url": "/resume-react/static/js/main.24bcb9c4.chunk.js"
+  },
+  {
+    "revision": "2eab88594988d414d350",
+    "url": "/resume-react/static/js/runtime-main.27456c11.js"
+  }
+]);
